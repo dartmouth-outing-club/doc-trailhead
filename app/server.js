69bf9930e78ccdb76c0a567a2faf00d3418690df
Tim Tregubov <tim@zingweb.com>
2017-05-03 14:40:39 -0400

    adding static

--- app/server.js
+++ app/server.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import path from 'path';
 
 // initialize
 const app = express();
 
 // enable/disable cross origin resource sharing if necessary
 app.use(cors());
+
+app.set('view engine', 'ejs');
+app.use(express.static('static'));
+// enables static assets from folder static
+app.set('views', path.join(__dirname, '../app/views'));
+// this just allows us to render ejs from the ../app/views directory
 
 // enable json message body for posting data to API
 app.use(bodyParser.urlencoded({ extended: true }));
