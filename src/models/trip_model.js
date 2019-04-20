de35ca4448aa65c483005071d000503cf96d49db
Annika Kouhia <annika.j.kouhia.20@dartmouth.edu>
2019-04-20 00:29:38 -0400

    making trips creation work AND creating the pending list

--- src/models/trip_model.js
+++ src/models/trip_model.js
@@ -4,8 +4,10 @@ import mongoose, { Schema } from 'mongoose';
 const TripSchema = new Schema({
   title: String,
   leaders: [{ type: Schema.Types.ObjectId, ref: 'User' }], // leaders
-  club: { type: Schema.Types.ObjectId, ref: 'Club' },
+  // club: { type: Schema.Types.ObjectId, ref: 'Club' },
+  club: { type: String, enum: ['Ledyard', 'CNT', 'DMC'], default: 'Ledyard' },
   members: [{ type: Schema.Types.ObjectId, ref: 'User' }], // users
+  pending: [{ type: Schema.Types.ObjectId, ref: 'User' }], // pending members
   startDate: Date,
   endDate: Date,
   cost: String,
