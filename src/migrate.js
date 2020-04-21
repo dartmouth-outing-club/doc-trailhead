6e7fd4d622ffff0a4f7b0fbcb5ad26d58383b433
ziruihao <ziray.hao.22@dartmouth.edu>
2020-04-20 20:36:44 -0400

    migrate script now saves globals

--- src/migrate.js
+++ src/migrate.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
-import Trips from './models/trip-model';
-import VehicleRequests from './models/vehicle-request-model';
+import Trip from './models/trip-model';
+import Global from './models/global-model';
+import VehicleRequest from './models/vehicle-request-model';
 
 const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/doc-planner';
 mongoose.set('useCreateIndex', true);
@@ -13,19 +14,37 @@ function migrateDB() {
    * PR#
    * Adds numbering to trips and vehicle requests.
    */
-  Trips.find({}).then(async (trips) => {
-    for (let i = 0; i < trips.length; i += 1) {
-      trips[i].number = i + 1;
-      // eslint-disable-next-line no-await-in-loop
-      await trips[i].save();
-    }
-  });
-  VehicleRequests.find({}).then(async (requests) => {
-    for (let i = 0; i < requests.length; i += 1) {
-      requests[i].number = i + 1;
-      // eslint-disable-next-line no-await-in-loop
-      await requests[i].save();
-    }
+  global = new Global();
+  global.tripNumberMax = 0;
+  global.vehicleRequestNumberMax = 0;
+  global.save().then(() => {
+    Trip.find({}).then(async (trips) => {
+      let final = 0;
+      for (let i = 0; i < trips.length; i += 1) {
+        trips[i].number = i + 1;
+        final = i + 1;
+        // eslint-disable-next-line no-await-in-loop
+        await trips[i].save();
+      }
+      Global.find({}).then((globals) => {
+        globals[0].tripNumberMax = final;
+        globals[0].save();
+      });
+    });
+    VehicleRequest.find({}).then(async (requests) => {
+      let final = 0;
+      for (let i = 0; i < requests.length; i += 1) {
+        requests[i].number = i + 1;
+        final = i + 1;
+        // eslint-disable-next-line no-await-in-loop
+        await requests[i].save();
+      }
+      Global.find({}).then((globals) => {
+        globals[0].vehicleRequestNumberMax = final;
+        globals[0].save();
+      });
+    });
+    console.log('seeded db. Press control+c to exit');
   });
 }
 
