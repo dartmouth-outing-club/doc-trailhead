24988461b8efb40ea201ca114be4ff98579f30b7
ziruihao <ziray.hao.22@dartmouth.edu>
2020-04-26 15:23:09 -0400

    added server constants

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const frontendURL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'http://doc-dartmouth.surge.sh';
+export const backendURL = process.env.NODE_ENV === 'development' ? 'http://localhost:9090/api' : 'https://doc-planner.herokuapp.com/api';
+
+export const hi = 'hi';
