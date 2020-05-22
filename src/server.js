3c1eb5b27ae55ec1fc34b5cdf831439013fcc0de
ziruihao <ziray.hao.22@dartmouth.edu>
2020-05-22 17:03:40 -0400

    initializing cron scheduler on server start

--- src/server.js
+++ src/server.js
@@ -6,9 +6,9 @@ import morgan from 'morgan';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import apiRouter from './router';
-import mailer from './services';
+import { mailer, scheduler } from './services';
 import * as constants from './constants';
-import tokenForUser from './controllers/user-controller';
+import { tokenForUser } from './controllers/user-controller';
 import Trip from './models/trip-model';
 
 
@@ -57,11 +57,19 @@ const port = process.env.PORT || 9090;
 app.listen(port);
 
 const sendCheckOutEmail = () => {
+  console.log('executing checkout send');
   const today = new Date();
   Trip.find({}).populate('leaders').then((trips) => {
     trips.forEach((trip) => {
-      if ((trip.startDate > today) && (trip.startDate.getTime() - today.getTime() < (48 * 3600)) && (trip.startDate.getTime() - today.getTime() > (24 * 3600))) {
+      console.log('startDate', trip.startDate.getTime());
+      console.log('today', today.getTime());
+      console.log('in the future', ((trip.startDate > today)));
+      console.log('less than 48', trip.startDate.getTime() - today.getTime() <= (48 * (3600000)));
+      console.log('more than 24', trip.startDate.getTime() - today.getTime() >= (24 * (3600000)));
+      if ((trip.startDate > today) && (trip.startDate.getTime() - today.getTime() <= (48 * 3600000)) && (trip.startDate.getTime() - today.getTime() >= (24 * 3600000))) {
+        console.log('should send email');
         const leaderEmails = trip.leaders.map((leader) => { return leader.email; });
+        console.log(trip.leaders[0]._id);
         mailer.send({ address: leaderEmails, subject: `Trip #${trip.number} is happening soon`, message: `Hello,\n\nYou're Trip #${trip.number}: ${trip.name} is happening in 48 hours!\n\nView the trip here: ${constants.frontendURL}/trip/${trip._id}\n\nIMPORTANT: on the day of the trip, you must mark all attendees here: ${constants.frontendURL}/trip-check-out/${trip._id}?token=${tokenForUser(trip.leaders[0], 'mobile', trip._id)}\n\nBest,\nDOC Planner` });
       }
     });
@@ -69,15 +77,29 @@ const sendCheckOutEmail = () => {
 };
 
 const sendCheckInEmail = () => {
+  console.log('executing checkin send');
   const today = new Date();
   Trip.find({}).populate('leaders').then((trips) => {
     trips.forEach((trip) => {
-      if ((trip.endDate > today) && (trip.endDate.getTime() - today.getTime() < (48 * 3600)) && (trip.endDate.getTime() - today.getTime() > (24 * 3600))) {
+      console.log('endDate', trip.endDate.getTime());
+      console.log('today', today.getTime());
+      console.log('less than 48', trip.endDate.getTime() - today.getTime() >= (48 * (3600000)));
+      console.log('more than 24', trip.endDate.getTime() - today.getTime() >= (24 * (3600000)));
+      if ((trip.endDate > today) && (trip.endDate.getTime() - today.getTime() < (48 * 3600000)) && (trip.endDate.getTime() - today.getTime() > (24 * 3600000))) {
+        console.log('should send email');
         const leaderEmails = trip.leaders.map((leader) => { return leader.email; });
+        console.log(trip.leaders[0]._id);
         mailer.send({ address: leaderEmails, subject: `Trip #${trip.number} should be returning soon`, message: `Hello,\n\nYou're Trip #${trip.number}: ${trip.name} is should return soon. If an EMERGENCY occured, please get emergency help right away, and follow the link below to mark your status so OPO staff is informed.\n\nIMPORTANT: right after you return, you must check-in all attendees here: ${constants.frontendURL}/trip-check-in/${trip._id}?token=${tokenForUser(trip.leaders[0], 'mobile', trip._id)}\n\nBest,\nDOC Planner` });
       }
     });
   });
 };
+
+// sendCheckOutEmail();
+// sendCheckInEmail();
+
+scheduler.schedule(sendCheckInEmail, 'minutely');
+scheduler.schedule(sendCheckOutEmail, 'minutely');
+scheduler.schedule(null, 'dnajndjad');
 
 console.log(`listening on: ${port}`);
