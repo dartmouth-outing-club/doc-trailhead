b76c1329a602188fdeb3294c29db3ff9081b16fa
ziruihao <ziray.hao.22@dartmouth.edu>
2020-05-22 18:27:39 -0400

    fleshed out other email functionality

--- src/server.js
+++ src/server.js
@@ -61,16 +61,13 @@ const sendCheckOutEmail = () => {
   const today = new Date();
   Trip.find({}).populate('leaders').then((trips) => {
     trips.forEach((trip) => {
-      console.log('startDate', trip.startDate.getTime());
-      console.log('today', today.getTime());
       console.log('in the future', ((trip.startDate > today)));
       console.log('less than 48', trip.startDate.getTime() - today.getTime() <= (48 * (3600000)));
       console.log('more than 24', trip.startDate.getTime() - today.getTime() >= (24 * (3600000)));
       if ((trip.startDate > today) && (trip.startDate.getTime() - today.getTime() <= (48 * 3600000)) && (trip.startDate.getTime() - today.getTime() >= (24 * 3600000))) {
-        console.log('should send email');
         const leaderEmails = trip.leaders.map((leader) => { return leader.email; });
         console.log(trip.leaders[0]._id);
-        mailer.send({ address: leaderEmails, subject: `Trip #${trip.number} is happening soon`, message: `Hello,\n\nYou're Trip #${trip.number}: ${trip.name} is happening in 48 hours!\n\nView the trip here: ${constants.frontendURL}/trip/${trip._id}\n\nIMPORTANT: on the day of the trip, you must mark all attendees here: ${constants.frontendURL}/trip-check-out/${trip._id}?token=${tokenForUser(trip.leaders[0], 'mobile', trip._id)}\n\nBest,\nDOC Planner` });
+        mailer.send({ address: leaderEmails, subject: `Trip #${trip.number} is happening soon`, message: `Hello,\n\nYour Trip #${trip.number}: ${trip.name} is happening in 48 hours!\n\nView the trip here: ${constants.frontendURL}/trip/${trip._id}\n\nIMPORTANT: on the day of the trip, you must mark all attendees here: ${constants.frontendURL}/trip-check-out/${trip._id}?token=${tokenForUser(trip.leaders[0], 'mobile', trip._id)}\n\nBest,\nDOC Planner` });
       }
     });
   });
@@ -81,15 +78,41 @@ const sendCheckInEmail = () => {
   const today = new Date();
   Trip.find({}).populate('leaders').then((trips) => {
     trips.forEach((trip) => {
-      console.log('endDate', trip.endDate.getTime());
-      console.log('today', today.getTime());
-      console.log('less than 48', trip.endDate.getTime() - today.getTime() >= (48 * (3600000)));
+      console.log('less than 48', trip.endDate.getTime() - today.getTime() <= (48 * (3600000)));
       console.log('more than 24', trip.endDate.getTime() - today.getTime() >= (24 * (3600000)));
       if ((trip.endDate > today) && (trip.endDate.getTime() - today.getTime() < (48 * 3600000)) && (trip.endDate.getTime() - today.getTime() > (24 * 3600000))) {
-        console.log('should send email');
         const leaderEmails = trip.leaders.map((leader) => { return leader.email; });
         console.log(trip.leaders[0]._id);
-        mailer.send({ address: leaderEmails, subject: `Trip #${trip.number} should be returning soon`, message: `Hello,\n\nYou're Trip #${trip.number}: ${trip.name} is should return soon. If an EMERGENCY occured, please get emergency help right away, and follow the link below to mark your status so OPO staff is informed.\n\nIMPORTANT: right after you return, you must check-in all attendees here: ${constants.frontendURL}/trip-check-in/${trip._id}?token=${tokenForUser(trip.leaders[0], 'mobile', trip._id)}\n\nBest,\nDOC Planner` });
+        mailer.send({ address: leaderEmails, subject: `Trip #${trip.number} should be returning soon`, message: `Hello,\n\nYour Trip #${trip.number}: ${trip.name} is should return soon. If an EMERGENCY occured, please get emergency help right away, and follow the link below to mark your status so OPO staff is informed.\n\nIMPORTANT: right after you return, you must check-in all attendees here: ${constants.frontendURL}/trip-check-in/${trip._id}?token=${tokenForUser(trip.leaders[0], 'mobile', trip._id)}\n\nBest,\nDOC Planner` });
+      }
+    });
+  });
+};
+
+const sendWarningEmail = () => {
+  console.log('executing warning send');
+  const today = new Date();
+  Trip.find({}).populate('leaders').then((trips) => {
+    trips.forEach((trip) => {
+      console.log('less than 3hr', trip.endDate.getTime() - today.getTime() <= (3 * (3600000)));
+      console.log('more than 90mins', trip.endDate.getTime() - today.getTime() >= (1.5 * (3600000)));
+      if ((trip.endDate > today) && (trip.endDate.getTime() - today.getTime() < (48 * 3600000)) && (trip.endDate.getTime() - today.getTime() > (1.5 * 3600000))) {
+        const leaderEmails = trip.leaders.map((leader) => { return leader.email; });
+        console.log(trip.leaders[0]._id);
+        mailer.send({ address: leaderEmails, subject: `Trip #${trip.number} late for return`, message: `Hello,\n\nYour Trip #${trip.number}: ${trip.name} is is now 90 minutes late. OPO will be notified in the next 90 minutes if your trip is not back in Hanover. If you are having difficulties getting back, please follow the DOC Emergency Protocols found here:\n\nhttps://docs.google.com/forms/u/1/d/e/1FAIpQLSeo9jIcTGNstZ1uADtovDjJT8kkPtS-YpRwzJC2MZkVkbH0hw/viewform.\n\nIMPORTANT: right after you return, you must check-in all attendees here: ${constants.frontendURL}/trip-check-in/${trip._id}?token=${tokenForUser(trip.leaders[0], 'mobile', trip._id)}\n\nBest,\nDOC Planner` });
+      }
+    });
+  });
+};
+
+const sendOPOEmail = () => {
+  console.log('executing warning send');
+  const today = new Date();
+  Trip.find({}).populate('leaders').then((trips) => {
+    trips.forEach((trip) => {
+      console.log('more than 3hr', trip.endDate.getTime() - today.getTime() >= (3 * (3600000)));
+      if ((trip.endDate > today) && (trip.endDate.getTime() - today.getTime() < (3 * 3600000)) && (trip.endDate.getTime() - today.getTime() > (1.5 * 3600000))) {
+        mailer.send({ address: constants.OPOEmails, subject: `Trip #${trip.number} not returned`, message: `Hello,\n\nTrip ${trip.number}: ${trip.title}, was due back at ${trip.endDate} and has not yet checked back in from Hanover. Trip details can be found at:\n\n${constants.frontendURL}/trip/${trip._id}\n\nBest,\nDOC Planner` });
       }
     });
   });
@@ -97,9 +120,10 @@ const sendCheckInEmail = () => {
 
 // sendCheckOutEmail();
 // sendCheckInEmail();
+// sendOPOEmail();
 
-scheduler.schedule(sendCheckInEmail, 'minutely');
-scheduler.schedule(sendCheckOutEmail, 'minutely');
-scheduler.schedule(null, 'dnajndjad');
+// scheduler.schedule(sendCheckInEmail, 'minutely');
+// scheduler.schedule(sendCheckOutEmail, 'minutely');
+// scheduler.schedule(null, 'dnajndjad');
 
 console.log(`listening on: ${port}`);
