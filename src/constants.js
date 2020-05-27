ab2e66ee04f6af8c653c546ea3828108310fc4b1
ziruihao <ziray.hao.22@dartmouth.edu>
2020-05-27 14:34:28 -0400

    url fix

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const frontendURL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'http://doc-dartmouth.surge.sh';
-export const backendURL = process.env.NODE_ENV === 'development' ? 'http://localhost:9090/api' : 'https://doc-planner.herokuapp.com/api';
+export const backendURL = process.env.NODE_ENV === 'development' ? 'http://localhost:9090/api' : 'https://doc-dartmouth.herokuapp.com/api';
 
 export const hi = 'hi';
 
