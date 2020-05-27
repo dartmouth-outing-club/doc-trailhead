ec6d2076464e301c3ebd0b40ab11f73f40fba224
ziruihao <ziray.hao.22@dartmouth.edu>
2020-05-27 14:42:40 -0400

    adding global migration to seed script

--- src/db/seed.js
+++ src/db/seed.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import Globals from '../models/global-model';
 import Clubs from '../models/club-model';
 import Users from '../models/user-model';
 import Trips from '../models/trip-model';
@@ -330,69 +331,79 @@ const vehicles = [
 ];
 
 function seedDB() {
-  VehicleRequests.deleteMany({})
-    .then(() => {
-      Assignmnets.deleteMany({})
-        .then(() => {
-          Vehicles.deleteMany({})
-            .then(() => {
-              Clubs.deleteMany({})
-                .then(() => {
-                  Trips.deleteMany({})
-                    .then(() => {
-                      Users.deleteMany({})
-                        .then(() => {
-                          Users.deleteMany({})
-                            .then(() => {
-                              Vehicles.insertMany(vehicles)
-                                .then(() => {
-                                  Clubs.insertMany(clubs)
-                                    .then((insertedClubs) => {
-                                      fakeUsers.forEach((fakeUser) => {
-                                        const newUser = new Users();
-                                        newUser.casID = fakeUser.casID;
-                                        newUser.email = fakeUser.email;
-                                        newUser.password = fakeUser.password;
-                                        newUser.name = fakeUser.name;
-                                        newUser.role = fakeUser.role;
-                                        newUser.dash_number = fakeUser.dash_number;
-                                        newUser.driver_cert = fakeUser.driver_cert;
-                                        newUser.trailer_cert = fakeUser.trailer_cert;
+  Globals.deleteMany({}).then(() => {
+    VehicleRequests.deleteMany({})
+      .then(() => {
+        Assignmnets.deleteMany({})
+          .then(() => {
+            Vehicles.deleteMany({})
+              .then(() => {
+                Clubs.deleteMany({})
+                  .then(() => {
+                    Trips.deleteMany({})
+                      .then(() => {
+                        Users.deleteMany({})
+                          .then(() => {
+                            Users.deleteMany({})
+                              .then(() => {
+                                const global = new Globals();
+                                global.tripNumberMax = 0;
+                                global.vehicleRequestNumberMax = 0;
+                                global.save().then(() => {
+                                  Vehicles.insertMany(vehicles)
+                                    .then(() => {
+                                      Clubs.insertMany(clubs)
+                                        .then((insertedClubs) => {
+                                          Promise.all(
+                                            fakeUsers.map((fakeUser) => {
+                                              return new Promise((resolve, reject) => {
+                                                const newUser = new Users();
+                                                newUser.casID = fakeUser.casID;
+                                                newUser.email = fakeUser.email;
+                                                newUser.password = fakeUser.password;
+                                                newUser.name = fakeUser.name;
+                                                newUser.role = fakeUser.role;
+                                                newUser.dash_number = fakeUser.dash_number;
+                                                newUser.driver_cert = fakeUser.driver_cert;
+                                                newUser.trailer_cert = fakeUser.trailer_cert;
 
-                                        newUser.photo_url = 'https://i.pinimg.com/originals/aa/05/23/aa05237847e53e19a5d0deef64c33b79.jpg';
-                                        newUser.pronoun = 'they/them/their';
-                                        newUser.clothe_size = 'Men-S';
-                                        newUser.shoe_size = 'Women-2';
-                                        newUser.height = '6';
+                                                newUser.photo_url = 'https://i.pinimg.com/originals/aa/05/23/aa05237847e53e19a5d0deef64c33b79.jpg';
+                                                newUser.pronoun = 'they/them/their';
+                                                newUser.clothe_size = 'Men-S';
+                                                newUser.shoe_size = 'Women-2';
+                                                newUser.height = '6';
 
-                                        newUser.allergies_dietary_restrictions = 'none';
-                                        newUser.medical_conditions = 'Not a real human';
-                                        if (fakeUser.role === 'Leader' || fakeUser.role === 'OPO') {
-                                          const clubIds = [];
-                                          insertedClubs.forEach((insertedClub) => {
-                                            clubIds.push(insertedClub._id);
+                                                newUser.allergies_dietary_restrictions = 'none';
+                                                newUser.medical_conditions = 'Not a real human';
+                                                if (fakeUser.role === 'Leader' || fakeUser.role === 'OPO') {
+                                                  const clubIds = [];
+                                                  insertedClubs.forEach((insertedClub) => {
+                                                    clubIds.push(insertedClub._id);
+                                                  });
+                                                  newUser.leader_for = clubIds;
+                                                } else {
+                                                  newUser.leader_for = fakeUser.leader_for;
+                                                }
+                                                newUser.save().then(() => { return resolve(); });
+                                              });
+                                            }),
+                                          ).then(() => {
+                                            console.log('seeded db. Press control+c to exit');
+                                            // process.exit();
                                           });
-                                          newUser.leader_for = clubIds;
-                                        } else {
-                                          newUser.leader_for = fakeUser.leader_for;
-                                        }
-                                        newUser.save();
-                                      });
+                                        });
                                     });
                                 });
-                            })
-                            .then(() => {
-                              console.log('seeded db. Press control+c to exit');
-                              // process.exit();
-                            });
-                        });
-                    });
-                });
-            });
-        });
-    }).catch((error) => {
-      console.log(error);
-    });
+                              });
+                          });
+                      });
+                  });
+              });
+          });
+      }).catch((error) => {
+        console.log(error);
+      });
+  });
 }
 
 seedDB();
