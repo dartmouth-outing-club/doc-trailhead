5ac55d1b6fe1ade42c8fef87fc78d9ffed2b5528
ziruihao <ziray.hao.22@dartmouth.edu>
2020-05-27 14:34:34 -0400

    url fix

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,5 @@
 export const frontendURL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'http://doc-dartmouth.surge.sh';
 export const backendURL = process.env.NODE_ENV === 'development' ? 'http://localhost:9090/api' : 'https://doc-dartmouth.herokuapp.com/api';
 
-export const hi = 'hi';
-
 export const OPOEmails = ['rory.c.gawler@dartmouth.edu'];
 // export const OPOEmails = ['ziray.hao.22@dartmouth.edu'];
