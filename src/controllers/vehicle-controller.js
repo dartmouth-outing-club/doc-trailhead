7d28aa232d1bbe2b21778723e32145a10df6b6f8
ziruihao <ziray.hao.22@dartmouth.edu>
2020-06-05 17:10:18 -0400

    decomissioning vehicles instead of deleting them

--- src/controllers/vehicle-controller.js
+++ src/controllers/vehicle-controller.js
@@ -80,7 +80,7 @@ export const getVehicles = (req, res) => {
     })
     .exec()
     .then((vehicles) => {
-      res.json(vehicles);
+      res.json(vehicles.filter((vehicle) => { return vehicle.active; }));
     })
     .catch((error) => {
       res.status(500).send(error);
@@ -107,13 +107,17 @@ export const updateVehicle = (req, res) => {
 export const deleteVehicle = (req, res) => {
   Vehicle.findById(req.params.id).populate('bookings')
     .then((vehicle) => {
-      Vehicle.deleteOne({ _id: req.params.id }, (error) => {
-        if (error) {
-          res.json(error);
-          console.log(error);
-        } else {
-          res.json('Vehicle deleted');
-        }
+      vehicle.active = false;
+      // Vehicle.deleteOne({ _id: req.params.id }, (error) => {
+      //   if (error) {
+      //     res.json(error);
+      //     console.log(error);
+      //   } else {
+      //     res.json('Vehicle deleted');
+      //   }
+      // });
+      vehicle.save().then(() => {
+        res.json('Vehicle decomissioned');
       });
     })
     .catch((error) => {
