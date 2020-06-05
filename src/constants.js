bb6c69f80a0f50347ac23fcf0ff16ef868c65402
ziruihao <ziray.hao.22@dartmouth.edu>
2020-06-04 23:55:30 -0400

    rory email change

--- src/constants.js
+++ src/constants.js
@@ -5,8 +5,8 @@ dotenv.config({ silent: true });
 export const frontendURL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'http://doc-dartmouth.surge.sh';
 export const backendURL = process.env.NODE_ENV === 'development' ? 'http://localhost:9090/api' : 'https://doc-dartmouth.herokuapp.com/api';
 
-export const OPOEmails = ['rory.c.gawler@dartmouth.edu'];
-// export const OPOEmails = ['ziray.hao.22@dartmouth.edu'];
+// export const OPOEmails = ['rory.c.gawler@dartmouth.edu'];
+export const OPOEmails = ['ziray.hao.22@dartmouth.edu'];
 
 export const createDateObject = (date, time) => {
   // adapted from https://stackoverflow.com/questions/2488313/javascripts-getdate-returns-wrong-date
