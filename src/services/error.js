4be09bef5f600230e021241fe8eb0bad6974a5ae
ziruihao <ziray.hao.22@dartmouth.edu>
2020-09-15 18:57:01 -0400

    Built error schema

---
+++ src/services/error.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from 'mongoose';
+
+const ErrorSchema = new Schema({
+  type: String,
+  occuredDate: Date,
+  message: String,
+});
+
+const ErrorModel = mongoose.model('Error', ErrorSchema);
+
+const logError = (error) => {
+  const errorLog = new ErrorModel();
+  errorLog.type = error.type;
+  errorLog.message = error.message;
+  errorLog.occuredDate = new Date();
+  errorLog.save();
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export { logError };
