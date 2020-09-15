44c8fcaaa84999a0aace197f56da0e33bfc1e7da
ziruihao <ziray.hao.22@dartmouth.edu>
2020-09-15 18:57:14 -0400

    Converted nodemailer send function to return a promise

--- src/services/emailing.js
+++ src/services/emailing.js
@@ -14,18 +14,22 @@ const transporter = nodemailer.createTransport({
 });
 
 const send = (email) => {
-  const mailOptions = {
-    from: process.env.EMAIL_ADDRESS,
-    to: email.address,
-    subject: email.subject,
-    text: email.message,
-  };
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(`Email sent: ${info.response}`);
-    }
+  return new Promise((resolve, reject) => {
+    const mailOptions = {
+      from: process.env.EMAIL_ADDRESS,
+      to: email.address,
+      subject: email.subject,
+      text: email.message,
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log(`Email sent: ${info.response}`);
+        resolve();
+      }
+    });
   });
 };
 
