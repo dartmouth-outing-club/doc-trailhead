ea30e18ffe871fa5a556e2a6c0e8f5cc04fca446
ziruihao <ziray.hao.22@dartmouth.edu>
2020-09-25 17:17:30 -0600

    Removing OPO emailing list development

--- src/constants.js
+++ src/constants.js
@@ -6,7 +6,7 @@ dotenv.config({ silent: true });
 export const frontendURL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'http://doc.dartmouth.edu';
 export const backendURL = process.env.NODE_ENV === 'development' ? 'http://localhost:9090' : 'https://doc-planner.herokuapp.com';
 
-export const OPOEmails = ['rory.c.gawler@dartmouth.edu', 'willow.nilsen@dartmouth.edu', 'coz.teplitz@dartmouth.edu', 'gunnar.w.johnson@dartmouth.edu'];
+export const OPOEmails = process.env.NODE_ENV === 'development' ? ['ziray.hao@dali.dartmouth.edu'] : ['rory.c.gawler@dartmouth.edu', 'willow.nilsen@dartmouth.edu', 'coz.teplitz@dartmouth.edu', 'gunnar.w.johnson@dartmouth.edu'];
 
 export const createDateObject = (date, time, timezone) => {
   // adapted from https://stackoverflow.com/questions/2488313/javascripts-getdate-returns-wrong-date
