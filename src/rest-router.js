101ad855586cce287ce80787fc671895d0319ca6
alexpetros <apetros15@gmail.com>
2023-01-02 12:34:23 -0500

    Add homepage stylings

--- src/rest-router.js
+++ src/rest-router.js
@@ -1,11 +1,8 @@
 import { Router } from 'express'
 import * as index from './rest/index.js'
-import * as login from './rest/login.js'
 
 const router = Router()
 
 router.get('/index', index.get)
-
-router.get('/login', login.post)
 
 export default router
