000ec2ace9c220cbec6f4fa5316c5ebc9ebd2fb7
alexpetros <apetros15@gmail.com>
2023-01-31 17:02:02 -0500

    Add general-purpose ed script

---
+++ scripts/edit-templates.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+if [[ ! -n "${1-}" ]]; then
+  >&2 echo "Usage: edit-templates.sh SCRIPT NAME"
+  exit 1
+fi
+
+scriptname=$1
+if [[ ! -f "$scriptname" ]]; then
+  >&2 echo "Script $scriptname not found"
+  exit 1
+fi
+
+find ./templates/views -name '*.njs' -exec sh -c "ed {} < $scriptname" \;
