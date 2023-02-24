f0a4fbd67da807199a14b902568f365713b47d35
alexpetros <apetros15@gmail.com>
2023-02-24 16:27:12 -0500

    Add individual gear to OPO trip approvals table

--- src/views/opo/trip-approvals.js
+++ src/views/opo/trip-approvals.js
@@ -8,7 +8,6 @@ const OPO_TRIPS_QUERY = `
       location,
       start_time,
       users.name as owner,
-      clubs.name as club,
       group_gear_approved,
       iif(vr.id IS NULL,
         'N/A',
@@ -18,14 +17,18 @@ const OPO_TRIPS_QUERY = `
         'N/A',
         iif(pc.is_approved IS NULL, 'pending', iif(pc.is_approved = 0, 'denied', 'approved'))
       ) AS pc_status,
+    iif(trips.member_gear_approved IS NULL,
+      iif(mg.count IS NULL, 'N/A', 'pending'),
+      iif(trips.member_gear_approved = 1, 'approved', 'denied')) as mg_status,
     iif(trips.group_gear_approved IS NULL,
       iif(gg.count IS NULL, 'N/A', 'pending'),
       iif(trips.group_gear_approved = 1, 'approved', 'denied')) as gg_status
     FROM trips
     LEFT JOIN users on trips.owner = users.id
-    LEFT JOIN clubs ON trips.club = clubs.id
     LEFT JOIN trip_pcard_requests AS pc ON pc.trip = trips.id
     LEFT JOIN vehiclerequests AS vr ON vr.trip = trips.id
+    LEFT JOIN (SELECT trip, count(*) as count FROM member_gear_requests GROUP BY trip) AS mg
+      ON mg.trip = trips.id
     LEFT JOIN (SELECT trip, count(*) as count FROM group_gear_requests GROUP BY trip) AS gg
       ON gg.trip = trips.id
 `
@@ -38,14 +41,15 @@ export function get (_req, res) {
     `${OPO_TRIPS_QUERY}
     WHERE start_time > @low_time
       AND start_time < @high_time
-      AND (gg_status != 'N/A' OR pc_status != 'N/A' OR vr_status != 'N/A')
+      AND (mg_status != 'N/A' OR gg_status != 'N/A' OR pc_status != 'N/A' OR vr_status != 'N/A')
     ORDER BY start_time DESC`,
     { low_time: pastTimeWindow.getTime(), high_time: now.getTime() }
   ).map(convertToRow)
 
   const future_trips = sqlite.all(
     `${OPO_TRIPS_QUERY}
-    WHERE start_time > ? AND (gg_status != 'N/A' OR pc_status != 'N/A' OR vr_status != 'N/A')
+    WHERE start_time > ?
+      AND (mg_status != 'N/A' OR gg_status != 'N/A' OR pc_status != 'N/A' OR vr_status != 'N/A')
     ORDER BY start_time ASC`,
     now.getTime()
   ).map(convertToRow)
@@ -57,9 +61,9 @@ function convertToRow (trip) {
   return {
     id: trip.id,
     title: trip.title,
-    club: trip.club,
     owner: trip.owner,
     start_time_element: utils.getDatetimeElement(trip.start_time),
+    mg_status_element: utils.getBadgeImgElement(trip.mg_status),
     gg_status_element: utils.getBadgeImgElement(trip.gg_status),
     vr_status_element: utils.getBadgeImgElement(trip.vr_status),
     pc_status_element: utils.getBadgeImgElement(trip.pc_status)
