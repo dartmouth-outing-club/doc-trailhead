b090fe7bb23031f5ae923b442386d2b12f2453ad
alexpetros <apetros15@gmail.com>
2023-02-25 11:15:01 -0500

    Show private trips in All Trips for OPO

--- src/views/all-trips.js
+++ src/views/all-trips.js
@@ -5,13 +5,15 @@ import { getClubIcon } from '../utils.js'
 export function get (req, res) {
   const now = new Date()
   const show_list = req.query?.view === 'list'
+  const showPrivate = res.locals.is_opo
+
   const publicTrips = sqlite.all(`
     SELECT trips.id, title, users.name as owner, location, start_time, end_time, description,
       clubs.name as club
     FROM trips
     LEFT JOIN users on trips.owner = users.id
     LEFT JOIN clubs on trips.club = clubs.id
-    WHERE start_time > ? AND private = 0
+    WHERE start_time > ? ${showPrivate ? '' : 'AND private = 0'}
     ORDER BY start_time ASC
   `, now.getTime())
 
