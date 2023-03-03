be913defde5a41f86355853999914c4c06d2ab76
Alexander Petros <apetros15@gmail.com>
2023-03-03 13:45:53 -0500

    Print server logs to STDERR if test suites fails

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 set -euo pipefail
 
+# When the script exits, delete the test artifacts and kill any child processes
 function cleanup {
-  rm -f *-test.db*
+  rm -f *-test.db* test-out
   jobs -p | xargs -r kill
 }
-
-# Ensures that the node server won't keep running after the script exists
 trap cleanup EXIT
+
+# If the tests didn't succeed, print the server logs and exit with an error
+function failTest {
+  cat >&2 <<EOF
+***********************************
+Test failed, outputting server logs
+***********************************
+EOF
+  cat test-out
+  exit 1
+}
 
 echo Initializing database...
 ./db/init-db.sh -fst
 
 echo Starting server...
-NODE_ENV=TESTS node src/server.js &> /dev/null &
+NODE_ENV=TESTS node src/server.js &> test-out &
 sleep 1
 
-node --test
+node --test || failTest
