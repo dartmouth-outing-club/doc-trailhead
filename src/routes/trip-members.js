e9e7e9ec5cea56da940c8e49ece3086e9542c686
ziruihao <ziray.hao@gmail.com>
2023-04-08 17:41:14 -0400

    Add trip roster auto-approval logic

--- src/routes/trip-members.js
+++ src/routes/trip-members.js
@@ -1,6 +1,7 @@
 import * as emails from '../emails.js'
 import * as mailer from '../services/mailer.js'
 import * as tripCard from '../routes/trip-card.js'
+import { _24_HOURS_IN_MS } from '../constants.js'
 
 export function makeLeader (req, res) {
   const { tripId, userId } = req.params
@@ -32,8 +33,20 @@ export function sendToWaitlist (req, res) {
   const owner = req.db.get('SELECT owner FROM trips WHERE id = ?', tripId).owner
   if (userId === owner) return res.sendStatus(400)
 
-  req.db.run('UPDATE trip_members SET pending = 1 WHERE trip = ? and user = ?', tripId, userId)
+  /**
+   * This 3-step process ensures that the newly removed user gets sent to the "back of the line",
+   * so they don't get re-auto-approved instantly, allowing
+   * the next person in line to be auto-approved.
+   */
+  // temporarily delete trip_member row
+  req.db.run('DELETE FROM trip_members WHERE trip = ? and user = ?', tripId, userId)
+  // auto-approve next person in line
+  autoApprove(tripId)(req.db)
+  // re-add user to trip_members with pending = 1
+  req.db.run('INSERT INTO trip_members (trip, user, leader, pending) VALUES (?, ?, ?, ?)', tripId, userId, 0, 1)
+
   mailer.send(emails.getTripRemovalEmail, req.db, tripId, userId)
+
   return tripCard.renderLeaderCard(req, res, tripId, userId)
 }
 
@@ -74,6 +87,7 @@ export function signup (req, res) {
   // otherwise it means they changed their gear request
   if (info.changes === 1) {
     mailer.send(emails.getTripApplicationConfirmation, req.db, tripId, req.user)
+    autoApprove(tripId)(req.db)
   } else {
     mailer.send(emails.getGearRequestChangedEmail, req.db, tripId, req.user)
   }
@@ -89,11 +103,46 @@ export function leave (req, res) {
   if (!tripId) return res.sendStatus(400)
   if (req.user === owner) return res.sendStatus(400)
 
+  // check if user was approved
+  const { pending } = req.db.get('SELECT pending FROM trip_members WHERE trip = ? and user = ?', tripId, req.user)
+
   const { changes } = req.db.run('DELETE FROM trip_members WHERE trip = ? and user = ?',
     tripId, req.user)
   if (changes === 0) console.warn(`Unnecessary delete requested for trip ${tripId}`)
 
-  // TODO only send if user was approved
-  mailer.send(emails.getUserLeftEmail, req.db, tripId, req.user)
+  if (!pending) {
+    mailer.send(emails.getUserLeftEmail, req.db, tripId, req.user)
+    autoApprove(tripId)(req.db)
+  }
   return tripCard.renderSignupCard(req, res, tripId, req.user)
+}
+
+export function autoApprove (tripId) {
+  return (db) => {
+    let moreToApprove = true
+    while (moreToApprove) {
+      // fetch the current member count
+      const { count } = db.get('SELECT COUNT(*) as count FROM trip_members WHERE trip = ? AND pending = 0', tripId)
+      // fetch the auto_approved_members limit and the trip start time
+      const { auto_approved_members, start_time } = db.get('SELECT auto_approved_members, start_time FROM trips WHERE id = ?', tripId)
+
+      if (count < auto_approved_members && start_time - new Date().getTime() > _24_HOURS_IN_MS) {
+        // pull the top person from the waitlist
+        const waitlist = db.all(`
+          SELECT user FROM trip_members
+          WHERE trip = ? AND pending = 1
+          ORDER BY added_at ASC LIMIT 1
+        `, tripId)
+        if (waitlist.length > 0) {
+          const nextUser = waitlist[0].user
+          db.run('UPDATE trip_members SET pending = 0 WHERE trip = ? and user = ?', tripId, nextUser)
+          mailer.send(emails.getTripApprovalEmail, db, tripId, nextUser)
+        } else {
+          moreToApprove = false
+        }
+      } else {
+        moreToApprove = false
+      }
+    }
+  }
 }
