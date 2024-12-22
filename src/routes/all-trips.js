005a152ca73bd55735e45f6180cec04caae63f06
Dylan <dylanfridman@gmail.com>
2024-12-22 12:27:36 -0300

    Filter for beginner friendly trips

--- src/routes/all-trips.js
+++ src/routes/all-trips.js
@@ -3,6 +3,7 @@ import * as utils from '../utils.js'
 export function get(req, res) {
   const now = new Date()
   const show_list = req.query?.view === 'list'
+  const beginners_only = req.query?.beginnersOnly === 'true'
   const showPrivate = res.locals.is_opo
 
   const publicTrips = req.db.all(`
@@ -11,11 +12,13 @@ export function get(req, res) {
     FROM trips
     LEFT JOIN users on trips.owner = users.id
     LEFT JOIN clubs on trips.club = clubs.id
-    WHERE start_time > ? ${showPrivate ? '' : 'AND private = 0'}
+    WHERE start_time > ?
+    ${showPrivate ? '' : 'AND private = 0'}
+    ${beginners_only ? 'AND experience_needed = 0' : ''}
     ORDER BY start_time ASC
   `, now.getTime())
 
   const trips = publicTrips.map(utils.formatForTripForTables)
 
-  res.render('views/all-trips.njk', { trips, show_list })
+  res.render('views/all-trips.njk', { trips, show_list, beginners_only })
 }
