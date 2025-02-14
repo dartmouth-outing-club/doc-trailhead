47a5ce519a3bb5d77561f42af767f4620dc139c3
Dara Casey <dara.e.casey.25@dartmouth.edu>
2025-02-14 11:01:22 -0500

    added led by me check box to my trips / past trips nav bar

--- src/routes/my-past-trips.js
+++ src/routes/my-past-trips.js
@@ -1,30 +1,30 @@
-import * as utils from '../utils.js'
+import * as utils from '../utils.js';
 
 export function get(req, res) {
-  const userId = req.user
-  const _24_HOURS_IN_MS = 86400000
-  const now = new Date()
+    const userId = req.user;
+    const now = new Date();
 
-  const is_leader = req.db.get(`
-    SELECT 1 as is_leader
-    FROM club_leaders WHERE user = ? and is_approved = TRUE`, req.user)?.is_leader === 1
-  const can_create_trip = res.locals.is_opo || is_leader
+    const leaderOnly = req.query.leaderOnly === 'true';
 
-  const tripsForUser = req.db.all(`
-    SELECT trips.id, title, location, start_time, end_time, description,
-    coalesce(clubs.name, 'None') as club, leader
-    FROM trip_members
-    LEFT JOIN trips ON trips.id = trip_members.trip
-    LEFT JOIN clubs ON trips.club = clubs.id
-    WHERE trip_members.user = ? AND end_time < ?
-    ORDER BY start_time DESC
-  `, userId, now.getTime() - _24_HOURS_IN_MS)
+    const tripsQuery = `
+      SELECT trips.id, title, location, start_time, end_time, description,
+      COALESCE(clubs.name, 'None') as club
+      FROM trip_members
+      JOIN trips ON trips.id = trip_members.trip
+      LEFT JOIN clubs ON trips.club = clubs.id
+      WHERE trip_members.user = ? 
+      AND end_time < ?
+      ${leaderOnly ? 'AND trip_members.leader = 1' : ''}
+      ORDER BY end_time DESC
+    `;
 
-  const trips = tripsForUser.map(trip => ({
-    ...trip,
-    iconPath: utils.getClubIcon(trip.club),
-    time_element: utils.getDatetimeRangeElement(trip.start_time, trip.end_time)
-  }))
+    const tripsForUser = req.db.all(tripsQuery, userId, now.getTime());
+    
+    const trips = tripsForUser.map(trip => ({
+        ...trip,
+        iconPath: utils.getClubIcon(trip.club),
+        time_element: utils.getDatetimeRangeElement(trip.start_time, trip.end_time)
+    }));
 
-  res.render('views/my-past-trips.njk', { can_create_trip, trips })
+    res.render('views/my-past-trips.njk', { trips, leader_only: leaderOnly, show_all: !leaderOnly });
 }
