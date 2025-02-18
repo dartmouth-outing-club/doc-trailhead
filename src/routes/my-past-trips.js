6048ba8b2ee9ad2fde1da5e07d944c9ae80b9597
Dara Casey <dara.e.casey.25@dartmouth.edu>
2025-02-18 07:26:40 -0500

    linting:

--- src/routes/my-past-trips.js
+++ src/routes/my-past-trips.js
@@ -1,30 +1,31 @@
-import * as utils from '../utils.js';
+import * as utils from '../utils.js'
 
 export function get(req, res) {
-    const userId = req.user;
-    const now = new Date();
+  const userId = req.user
+  const now = new Date()
 
-    const leaderOnly = req.query.leaderOnly === 'true';
+  const leader_only = req.query.leader_only === 'true'
 
-    const tripsQuery = `
-      SELECT trips.id, title, location, start_time, end_time, description, leader,
-      COALESCE(clubs.name, 'None') as club
+  const tripsQuery = `
+      SELECT 
+        trips.id, title, location, start_time, end_time, description, leader,
+        coalesce(clubs.name, 'None') as club
       FROM trip_members
       JOIN trips ON trips.id = trip_members.trip
       LEFT JOIN clubs ON trips.club = clubs.id
-      WHERE trip_members.user = ? 
-      AND end_time < ?
-      ${leaderOnly ? 'AND trip_members.leader = 1' : ''}
+      WHERE
+        trip_members.user = ? 
+        AND end_time < ?
+        ${leader_only ? 'AND trip_members.leader = 1' : ''}
       ORDER BY end_time DESC
-    `;
+    `
 
-    const tripsForUser = req.db.all(tripsQuery, userId, now.getTime());
-    
-    const trips = tripsForUser.map(trip => ({
-        ...trip,
-        iconPath: utils.getClubIcon(trip.club),
-        time_element: utils.getDatetimeRangeElement(trip.start_time, trip.end_time)
-    }));
+  const trips = req.db.all(tripsQuery, userId, now.getTime())
+    .map(trip => ({
+      ...trip,
+      iconPath: utils.getClubIcon(trip.club),
+      time_element: utils.getDatetimeRangeElement(trip.start_time, trip.end_time)
+    }))
 
-    res.render('views/my-past-trips.njk', { trips, leader_only: leaderOnly, show_all: !leaderOnly });
+  res.render('views/my-past-trips.njk', { trips, leader_only })
 }
