33a704ff57d0a9b73b7b703f134d4911bfc9b060
caseydara <35241746+caseydara@users.noreply.github.com>
2025-02-19 00:40:16 -0500

    Upcoming / Past Trips Navigation Bar (#122)
    * changed nav bar for my trips page to have past trips/upcoming (similar to opo nav view)
    
    * added led by me check box to my trips / past trips nav bar
    
    * adding Dylan's new changes to profile
    
    * Revert "adding Dylan's new changes to profile"
    
    This reverts commit 21d8bede2f567a4780d7ba7c3364f71b3ad3c9e1.
    
    * added back create trip for leaders
    
    * fix to ensure leader view is available for leaders in trips
    
    * fixing code review comments
    
    * linting:
    
    * got rid of leader only

---
+++ src/routes/my-past-trips.js
@@ -0,0 +1,28 @@
+import * as utils from '../utils.js'
+
+export function get(req, res) {
+  const userId = req.user
+  const now = new Date()
+
+  const tripsQuery = `
+      SELECT 
+        trips.id, title, location, start_time, end_time, description, leader,
+        coalesce(clubs.name, 'None') as club
+      FROM trip_members
+      JOIN trips ON trips.id = trip_members.trip
+      LEFT JOIN clubs ON trips.club = clubs.id
+      WHERE
+        trip_members.user = ? 
+        AND end_time < ?
+      ORDER BY end_time DESC
+    `
+
+  const trips = req.db.all(tripsQuery, userId, now.getTime())
+    .map(trip => ({
+      ...trip,
+      iconPath: utils.getClubIcon(trip.club),
+      time_element: utils.getDatetimeRangeElement(trip.start_time, trip.end_time)
+    }))
+
+  res.render('views/my-past-trips.njk', { trips })
+}
--- src/routes/my-trips.js
+++ src/routes/my-trips.js
@@ -5,26 +5,36 @@ export function get(req, res) {
   const _24_HOURS_IN_MS = 86400000
   const now = new Date()
 
-  const is_leader = req.db.get(`
-    SELECT 1 as is_leader
-    FROM club_leaders WHERE user = ? and is_approved = TRUE`, req.user)?.is_leader === 1
+  const is_leader_query = `SELECT 1 as is_leader
+                           FROM club_leaders 
+                           WHERE user = ? and is_approved = TRUE`
+
+  const is_leader = req.db.get(is_leader_query, userId)?.is_leader === 1
+
   const can_create_trip = res.locals.is_opo || is_leader
 
-  const tripsForUser = req.db.all(`
-    SELECT trips.id, title, location, start_time, end_time, description,
-    coalesce(clubs.name, 'None') as club, leader
-    FROM trip_members
-    LEFT JOIN trips ON trips.id = trip_members.trip
-    LEFT JOIN clubs ON trips.club = clubs.id
-    WHERE trip_members.user = ? AND end_time > ?
-    ORDER BY start_time ASC
-  `, userId, now.getTime() - _24_HOURS_IN_MS)
+  const tripsQuery = `
+        SELECT 
+          trips.id, title, location, start_time, end_time, description, leader,
+          coalesce(clubs.name, 'None') as club
+        FROM trip_members
+        JOIN trips ON trips.id = trip_members.trip
+        LEFT JOIN clubs ON trips.club = clubs.id
+        WHERE 
+          trip_members.user = ?
+          AND end_time > ?
+        ORDER BY start_time ASC
+      `
 
-  const trips = tripsForUser.map(trip => ({
-    ...trip,
-    iconPath: utils.getClubIcon(trip.club),
-    time_element: utils.getDatetimeRangeElement(trip.start_time, trip.end_time)
-  }))
+  const trips = req.db.all(tripsQuery, userId, now.getTime() - _24_HOURS_IN_MS)
+    .map(trip => ({
+      ...trip,
+      iconPath: utils.getClubIcon(trip.club),
+      time_element: utils.getDatetimeRangeElement(trip.start_time, trip.end_time)
+    }))
 
-  res.render('views/my-trips.njk', { can_create_trip, trips })
+  res.render('views/my-trips.njk', {
+    trips,
+    can_create_trip
+  })
 }
