f733eaf287988a61a2799e4b90c284bcb307e0d9
Alexander Petros <contact@alexpetros.com>
2025-10-20 21:52:07 -0400

    Fix tests

--- db/seed-data/2-users.sql
+++ db/seed-data/2-users.sql
@@ -1,11 +1,11 @@
 INSERT INTO users
-  (cas_id,email,password,name,photo_url,pronoun,dash_number,allergies_dietary_restrictions,medical_conditions,clothe_size,shoe_size,height_inches,is_opo)
+  (cas_id,password,name,photo_url,pronoun,dash_number,allergies_dietary_restrictions,medical_conditions,clothe_size,shoe_size,height_inches,is_opo)
 VALUES
-  ('opo.user.1@DARTMOUTH.EDU','opo.user.1@dartmouth.edu',NULL,'OPO User 1',NULL,'He/Him/His','100131029','none','none','Men-L','Men-12','74',1),
-  ('leader.user.1@DARTMOUTH.EDU','leader.user.1@dartmouth.edu',NULL,'Leader User 1',NULL,'He / Him / His','100614391','None','None','Men-S','Men-9.5','72',0),
-  ('trippee.user.1@DARTMOUTH.EDU','trippee.user.1@dartmouth.edu',NULL,'Trippee User 1',NULL,'He / Him / His','10084918','None','None','Men-S','Men-9.5','72',0),
-  ('trippee.user.2@DARTMOUTH.EDU','trippee.user.2@dartmouth.edu',NULL,'Trippee User 2',NULL,'She / Her / Her','10013947','None','None','Women-M','Women-9.5','72',0),
-  ('trippee.user.3@DARTMOUTH.EDU','trippee.user.3@dartmouth.edu',NULL,'Trippee User 3',NULL,'They / Them / Their','100428466','None','None','Women-L','Women-9.5','68',0);
+  ('opo.user.1@DARTMOUTH.EDU',NULL,'OPO User 1',NULL,'He/Him/His','100131029','none','none','Men-L','Men-12','74',1),
+  ('leader.user.1@DARTMOUTH.EDU',NULL,'Leader User 1',NULL,'He / Him / His','100614391','None','None','Men-S','Men-9.5','72',0),
+  ('trippee.user.1@DARTMOUTH.EDU',NULL,'Trippee User 1',NULL,'He / Him / His','10084918','None','None','Men-S','Men-9.5','72',0),
+  ('trippee.user.2@DARTMOUTH.EDU',NULL,'Trippee User 2',NULL,'She / Her / Her','10013947','None','None','Women-M','Women-9.5','72',0),
+  ('trippee.user.3@DARTMOUTH.EDU',NULL,'Trippee User 3',NULL,'They / Them / Their','100428466','None','None','Women-L','Women-9.5','68',0);
 
 INSERT INTO user_certs VALUES
   (1,'MICROBUS',1),
