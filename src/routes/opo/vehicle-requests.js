8e3630aded29717757c804c8ffb899eaaa663e10
Ayden Hayes <adhayes1919@gmail.com>
2026-02-15 15:34:25 -0500

    vehicle requests show up as one row for each vehicle instead

--- src/routes/opo/vehicle-requests.js
+++ src/routes/opo/vehicle-requests.js
@@ -2,13 +2,16 @@ import * as utils from '../../utils.js'
 import { getBadgeImgElement } from '../../utils.js'
 
 export function get(req, res) {
-  //const requests = getRequestedVehicles(req).map(getRowData)
-  const requests = getVehicleRequests(req).map(getRowData)
-    console.log(requests)
+  //const requests = getVehicleRequests(req).map(getRowData)
+
+  const requests = getRequestedVehicles(req).map(getRowData)
+  //console.log(requests)
+
   const reviewed_requests = requests.filter(request => request.status !== 'pending')
   const pending_requests = requests.filter(request => request.status === 'pending')
+    //TODO: get status lmao
 
-  //res.render('views/opo/vehicle-requests.njk', { reviewed_requests, requests })
+  //res.render('views/opo/vehicle-requests.njk', { requests, requests })
   res.render('views/opo/vehicle-requests.njk', { reviewed_requests, pending_requests })
 }
 
@@ -38,34 +41,56 @@ function getVehicleRequests(req) {
     ORDER BY first_pickup ASC
 `, now.getTime())
 }
+/*
+ * For all requested vehicles, get trip info frm vehicle request 
+ *     trip.title 
+ *     and user from user.id = trip.owner
+ *      which is to say user.id = vehiclerequest.trip.owner 
+ *  NOTE: I think this could select from trips to start and have some nested json but that is nottt happening rn...
+ */
 
 function getRequestedVehicles(req) {
   const now = new Date()
     // select all info from 
-    let requested = []
     return req.db.all(`
-        SELECT vehiclerequest, pickup_time, return_time, mileage from requested_vehicles where pickup_time > ? `, now.getTime()
+        SELECT 
+               vehiclerequest, 
+               pickup_time, 
+               return_time, 
+               requested_vehicles.mileage,
+               users.name as requester_name,
+               trips.id as trip_id,
+               trips.title as reason,
+               iif(vehiclerequests.is_approved IS NULL, 'pending', iif(is_approved = 1, 'approved', 'denied')) as status
+        FROM requested_vehicles 
+        JOIN vehiclerequests ON vehiclerequests.id = requested_vehicles.vehiclerequest
+        JOIN trips ON trips.id = vehiclerequests.trip
+        JOIN users on users.id =  trips.owner 
+        where pickup_time > ? 
+        ORDER BY trip_id
+`, now.getTime()
     )
-
-    /*.forEach((vehicle, index) => {
-        const request = req.db.get(`SELECT trip, requester from vehiclerequests where id = ? AND is_approved = FALSE`, vehicle.vehiclerequest)
-        if (request){
-            const user = req.db.get(`SELECT name from users where id = ?`, request.requester)
-            const trip_name = req.db.get(`SELECT title from trips where id = ?`, request.trip)
-            const extended_vehicle = {..vehicle, user, trip_name}
-            requested.put(extended_vehicle)
-        }
-    })
+    /*
+      vehiclerequests.id,
+      users.name as requester_name,
+      //requested_vehicles.trip_mileage,
+      //requested_vehicles.average_mileage,
+      trips.id as trip_id,
+      iif(trips.id IS NOT NULL, trips.title, request_details) as reason,
+      first_pickup,
+      last_return,
+      requested_vehicles.vcount,
+      iif(is_approved IS NULL, 'pending', iif(is_approved = 1, 'approved', 'denied')) as status
     */
+    //TODO: check times
+
 }
 
 function getRowData(request) {
   return {
     ...request,
-    pickup_time_element: utils.getDatetimeElement(request.first_pickup),
-    return_time_element: utils.getDatetimeElement(request.last_return),
-    //pickup_time_element: utils.getDatetimeElement(request.pickup_time),
-    //return_time_element: utils.getDatetimeElement(request.return_time),
+    pickup_time_element: utils.getDatetimeElement(request.pickup_time),
+    return_time_element: utils.getDatetimeElement(request.return_time),
     status_element: getBadgeImgElement(request.status)
   }
 }
