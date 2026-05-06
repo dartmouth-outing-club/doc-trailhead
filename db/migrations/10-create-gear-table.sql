e418a91946e48d681b05acf6c28a1c858271ffc3
Ayden <ayden.hayes.26@dartmouth.edu>
2026-05-06 01:32:52 -0400

    putting all desired fields in manage gear page

--- db/migrations/10-create-gear-table.sql
+++ db/migrations/10-create-gear-table.sql
@@ -1,13 +1,17 @@
 -- TODO: each of these needs to have better data handling 
--- also eventully change type and class. maybe add descriptions/tags?
+-- also eventully change wording of type and class. maybe add descriptions/tags?
 CREATE TABLE gear (
   id INTEGER primary key,
   name TEXT UNIQUE NOT NULL,
   class TEXT NOT NULL,
+  description TEXT,
   type TEXT,
-  quantity integer NOT NULL,
-  active INTEGER DEFAULT TRUE
+  quantity INTEGER NOT NULL,
+  rental_fee INTEGER NOT NULL DEFAULT 0,
+  replacement_fee INTEGER NOT NULL DEFAULT 0
+  active INTEGER DEFAULT TRUE,
 ) STRICT;
+--TODO: add column for "available outside of DOC trips?"
 
 
 --alter table group_gear_requests add column is_approved integer NOT NULL default 0;
@@ -15,4 +19,3 @@ CREATE TABLE gear (
 
 --alter table group_gear_requests add column gear_id INTEGER references gear(id) on update cascade;
 --alter table member_gear_requests add column gear_id INTEGER references gear(id) on update cascade;
-
--- src/routes/opo/manage-gear.js
+++ src/routes/opo/manage-gear.js
@@ -1,25 +1,31 @@
 export function get(req, res) {
-  const gear = req.db.all('SELECT id, name, class, type, quantity FROM gear where active = 1')
-  return res.render('views/opo/manage-gear.njk', { gear })
+  const all_gear = req.db.all('SELECT id, name, class, description, type, quantity, rental_fee, replacement_fee FROM gear where active = 1')
+  return res.render('views/opo/manage-gear.njk', { all_gear })
 }
 
 export function post(req, res) {
     //TODO: had to rename class to gearclass here cause of JS conflict. should probbbbably rename elsewhere...
-  const { name, gearclass, type, quantity } = req.body
+  // NOTE: formating
+  const { name, gearclass, description, type, quantity, rental_fee, replacement_fee } = req.body
+  //NOTE: prolly only conflict on name AND class?
   req.db.run(
-    `INSERT INTO gear (name, class, type, quantity, active) 
-     VALUES (?, ?, ?, ?, 1) 
-     ON CONFLICT(name) 
-     DO UPDATE SET active = 1;`,
-    [name, gearclass, type, quantity]
+
+  `INSERT INTO gear (name, class, description, type, quantity, rental_fee, replacement_fee, active) 
+    VALUES (?, ?, ?, ?, ?, ?, ?, 1) 
+    ON CONFLICT(name) 
+    DO UPDATE SET 
+    description = excluded.description,
+    quantity = excluded.quantity,
+    rental_fee = excluded.rental_fee,
+    replacement_fee = excluded.replacement_fee,
+    active = 1;`,
+    [name, gearclass, description, type, quantity, rental_fee, replacement_fee]
   )
   return get(req, res)
 }
 
 export function del(req, res) {
-    console.log("called delete?")
   const id = req.params.id
-    console.log(req.params)
-  req.db.run('UPDATE gear SET active = 0 WHERE id = ?', id)
+  req.db.run('UPDATE gear SET active = false WHERE id = ?', id)
   return res.send('').status(200)
 }
