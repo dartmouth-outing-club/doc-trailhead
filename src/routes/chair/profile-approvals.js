bf694af9e86f845de3e408ebb5b945b7644dd76d
Alexander Petros <contact@alexpetros.com>
2026-05-25 13:01:50 -0400

    Use SQL date function

--- src/routes/chair/profile-approvals.js
+++ src/routes/chair/profile-approvals.js
@@ -3,7 +3,7 @@ import dateFormat from 'dateformat'
 export function get(req, res) {
   const userId = parseInt(req.user)
   const userChairIn = req.db.all(`
-    SELECT 
+    SELECT
         clubs.id,
         clubs.name
     FROM club_chairs
@@ -14,23 +14,20 @@ export function get(req, res) {
   const clubIds = userChairIn.map(({ id }) => id)
 
   const leadership_requests = req.db.all(`
-   SELECT 
-    club_leaders.rowid as req_id, 
-    users.name AS requester_name, 
-    clubs.name AS requested_item, 
-    clubs.id as clubid,
-    certs_med.type as medcert_type,
-    certs_med.expiration as medcert_expiration
-   FROM club_leaders
-   LEFT JOIN clubs ON clubs.id = club_leaders.club
-   LEFT JOIN users ON users.id = club_leaders.user
-   LEFT JOIN certs_med ON certs_med.user = club_leaders.user
-   WHERE opo_approved = 0 AND chair_approved = 0 AND clubid in (${clubIds.join(',')})
-   `).map(row => {
-    const date = new Date(row.medcert_expiration)
-    row.medcert_expiration = !isNaN(date.getTime()) ? dateFormat(date, 'mm-dd-yyyy') : null
-    return row
-  })
+     SELECT
+      club_leaders.rowid as req_id,
+      users.name AS requester_name,
+      clubs.name AS requested_item,
+      clubs.id as clubid,
+      certs_med.type as medcert_type,
+      date(certs_med.expiration/1000, 'unixepoch') as medcert_expiration
+     FROM club_leaders
+     LEFT JOIN clubs ON clubs.id = club_leaders.club
+     LEFT JOIN users ON users.id = club_leaders.user
+     LEFT JOIN certs_med ON certs_med.user = club_leaders.user
+     WHERE opo_approved = 0 AND chair_approved = 0 AND clubid in (${clubIds.join(',')})
+   `);
+
 
   return res.render('views/chair/profile-approvals.njk', { leadership_requests })
 }
